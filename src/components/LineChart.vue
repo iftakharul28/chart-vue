e913f5b7a05e041197ad6bd1ab03dd61e0e744d9
iftakharul28 <iftakharul28@gmail.com>
2022-09-20 23:03:27 +0600

    chart added

---
+++ src/components/LineChart.vue
@@ -0,0 +1,68 @@
+<script>
+import VueApexCharts from "vue3-apexcharts";
+export default {
+  components: {
+    apexcharts: VueApexCharts,
+  },
+  data() {
+    return {
+      chartOptions: {
+        chart: {
+          type: "area",
+          height: 350,
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          curve: "straight",
+        },
+        title: {
+          text: "Balances",
+          align: "center",
+          style: {
+            fontSize: "14px",
+          },
+        },
+        fill: {
+          opacity: 0.5,
+        },
+        grid: {
+          yaxis: {
+            lines: {
+              offsetX: -30,
+            },
+          },
+          padding: {
+            left: 20,
+          },
+        },
+      },
+      series: [
+        {
+          name: "series1",
+          data: [31, 40, 28, 51, 42, 109, 100],
+        },
+        {
+          name: "series2",
+          data: [11, 32, 45, 32, 34, 52, 41],
+        },
+        {
+          name: "series3",
+          data: [14, 36, 45, 32, 34, 5, 14],
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <apexcharts
+    width="550"
+    type="area"
+    :options="chartOptions"
+    :series="series"
+    class="home__left-chart"
+  />
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,78 +1,11 @@
-<script>
-import { Pie } from "vue-chartjs";
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  ArcElement,
-  CategoryScale,
-} from "chart.js";
-
-ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
-
-export default {
-  name: "PieChart",
-  components: {
-    Pie,
-  },
-  props: {
-    chartId: {
-      type: String,
-      default: "pie-chart",
-    },
-    width: {
-      type: Number,
-      default: 400,
-    },
-    height: {
-      type: Number,
-      default: 400,
-    },
-    cssClasses: {
-      default: "",
-      type: String,
-    },
-    styles: {
-      type: Object,
-      default: () => {},
-    },
-    plugins: {
-      type: Array,
-      default: () => [],
-    },
-  },
-  data() {
-    return {
-      chartData: {
-        labels: ["VueJs", "EmberJs", "ReactJs", "AngularJs"],
-        datasets: [
-          {
-            backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16"],
-            data: [40, 20, 80, 10],
-          },
-        ],
-      },
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-      },
-    };
-  },
-};
+<script setup>
+import DoughnutChart from "../components/DoughnutChart.vue";
+import LineChart from "../components/LineChart.vue";
 </script>
 
 <template>
-  <div>
-    <Pie
-      :chart-data="chartData"
-      :chart-options="chartOptions"
-      :chart-id="chartId"
-      :width="width"
-      :height="height"
-      :plugins="plugins"
-      :css-classes="cssClasses"
-      :styles="styles"
-    />
+  <div class="home__chart-list">
+    <LineChart />
+    <DoughnutChart />
   </div>
 </template>
